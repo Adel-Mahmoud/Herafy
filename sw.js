51d93fea69837a522e6cb9ca400fad3a73e558c8
adel <adelmahmoud75@gmail.com>
2026-04-05 20:21:57 +0200

    updated project

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "herafy-v1";
+const CACHE_NAME = "herafy-v3";
 
 const urlsToCache = [
   "/",
@@ -10,15 +10,42 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(cached => {
+      const fetchPromise = fetch(event.request).then(networkRes => {
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkRes.clone());
+        });
+        return networkRes;
+      }).catch(() => cached);
+
+      return cached || fetchPromise;
+    })
   );
+});
+
+self.addEventListener("message", event => {
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
 });
